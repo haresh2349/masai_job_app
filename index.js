e896a3fa335608ba0b2e09df5f7d0d955005af55
haresh2349 <hareshcsolanki07@gmail.com>
2022-11-08 11:20:59 +0530

    home route added

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { connectDB } = require("./config/db");
 const { JobRouter } = require("./route/jobPost.route");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 app.use("/jobPosts", JobRouter);
 app.listen(PORT, async (req, res) => {
   try {
